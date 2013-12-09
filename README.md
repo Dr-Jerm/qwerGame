2ba5d43a53c3f389e796e7e0cf96133e1ff0c2ab
Vagrant <vagrant@vagrant-ubuntu.vagrantup.com>
2013-12-09 00:25:26 -0200

    added image assets for dota. thats... about it

---
+++ README.md
@@ -0,0 +1,4 @@
+angularTicker
+=============
+
+RESTful stock ticker using node.js and angular.js
---
+++ app/js/app.js
@@ -0,0 +1,8 @@
+(function(){
+    var qwer = angular.module("qwerGame", []);
+
+    qwer.controller('mainNav', function ($scope) {
+        $scope.test = "sup";
+    })
+	console.log("Up and running!");
+})();
